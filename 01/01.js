8b85dc8c565712da6362a9e8c0c6e88be46a3f2a
julienbeauchant <julienbeauchant.pro@yahoo.com>
2024-08-22 10:14:02 +0200

    test

---
+++ 01/01.js
@@ -0,0 +1,7 @@
+for (let ligne = "#"; ligne.length <= 7; ligne += "#") {
+    console.log(ligne);
+}
+
+// déclaration de la variable et affectation en chaine de caractere #
+// la boucle continue tant que la longueur de la ligne est <= 7
+// # s'affiche à chaque boucles
